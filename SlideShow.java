0928f124d426632f4eb1b0fc7d3fd6ebd991fb3e
jaydeesan <67033826+jaydeesan@users.noreply.github.com>
2021-02-28 16:00:48 -0600

    Add files via upload

---
+++ SlideShow.java
@@ -0,0 +1,168 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class SlideShow extends JFrame {
+
+	//Declare Variables
+	private JPanel slidePane;
+	private JPanel textPane;
+	private JPanel buttonPane;
+	private CardLayout card;
+	private CardLayout cardText;
+	private JButton btnPrev;
+	private JButton btnNext;
+	private JLabel lblSlide;
+	private JLabel lblTextArea;
+
+	/**
+	 * Create the application.
+	 */
+	public SlideShow() throws HeadlessException {
+		initComponent();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initComponent() {
+		//Initialize variables to empty objects
+		card = new CardLayout();
+		cardText = new CardLayout();
+		slidePane = new JPanel();
+		textPane = new JPanel();
+		textPane.setBackground(Color.ORANGE);
+		textPane.setBounds(5, 470, 790, 50);
+		textPane.setVisible(true);
+		buttonPane = new JPanel();
+		btnPrev = new JButton();
+		btnNext = new JButton();
+		lblSlide = new JLabel();
+		lblTextArea = new JLabel();
+
+		//Setup frame attributes
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setTitle("Top 5 Destinations SlideShow");
+		getContentPane().setLayout(new BorderLayout(10, 50));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Setting the layouts for the panels
+		slidePane.setLayout(card);
+		textPane.setLayout(cardText);
+		
+		//logic to add each of the slides and text
+		for (int i = 1; i <= 5; i++) {
+			lblSlide = new JLabel();
+			lblTextArea = new JLabel();
+			lblSlide.setText(getResizeIcon(i));
+			lblTextArea.setText(getTextDescription(i));
+			slidePane.add(lblSlide, "card" + i);
+			textPane.add(lblTextArea, "cardText" + i);
+		}
+
+		getContentPane().add(slidePane, BorderLayout.CENTER);
+		getContentPane().add(textPane, BorderLayout.SOUTH);
+
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		btnPrev.setText("Previous");
+		btnPrev.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goPrevious();
+			}
+		});
+		buttonPane.add(btnPrev);
+
+		btnNext.setText("Next");
+		btnNext.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goNext();
+			}
+		});
+		buttonPane.add(btnNext);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Previous Button Functionality
+	 */
+	private void goPrevious() {
+		card.previous(slidePane);
+		cardText.previous(textPane);
+	}
+	
+	/**
+	 * Next Button Functionality
+	 */
+	private void goNext() {
+		card.next(slidePane);
+		cardText.next(textPane);
+	}
+
+	/** Here I imported the images to the source folder. I thought of the upcoming trips that I wanted to take to various spas. Then I changed the resource images from the tester images to the ones I saved from Google. 
+	 */	
+	private String getResizeIcon(int i) {
+		String image = ""; 
+		if (i==1){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/TestImage1.jpg") + "'</body></html>";
+		} else if (i==2){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/COMO Bali.jpg") + "'</body></html>";
+		} else if (i==3){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Burgenstock hotel.jpg") + "'</body></html>";
+		} else if (i==4){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Lake Austin Spa Resort.jpg") + "'</body></html>";
+		} else if (i==5){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Amangiri resort.jpg") + "'</body></html>";
+		}
+		return image;
+	}
+	
+	
+	/**
+	 * To get the font displayed as is, I used the first travel spot "The Grand Canyon" as precedent for the following images. I tweeked the font slightly by 6 size so it was a little bigger, bringing more attention to the sites. 
+	 */
+	private String getTextDescription(int i) {
+		String text = ""; 
+		if (i==1){
+			text = "<html><body><font size='6'>#1 The Grand Canyon.</font> <br><Font size='4'>Spectacular canyon views and hiking.</font></body></html>";
+		} else if (i==2){
+			text = "<html><body><font size='6'>#2 COMO Bali.</font> <br><Font size='4'>Take a break to the origin of relaxation and meditation.</font></body></html>";
+		} else if (i==3){
+			text = "<html><body><font size='6'>#3 Burgenstock Hotel</font> <br><Font size='4'>The beautiful Switzerland mountains.</font></body></html>";
+		} else if (i==4){
+			text = "<html><body><font size='6'>#4 Lake Austin Spa Resort</font> <br><Font size='4'>Crystal clear lake with peaceful bonfires.</font></body></html>";
+		} else if (i==5){
+			text = "<html><body><font size='6'>#5 Amangiri Resort</font> <br><Font size='4'>Salt Utah mountains and beautiful pools.</font></body></html>";
+		}
+		return text;
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SlideShow ss = new SlideShow();
+				ss.setVisible(true);
+			}
+		});
+	}
+}
